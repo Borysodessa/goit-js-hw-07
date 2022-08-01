c0675a458e242048b00980446f477faa8b7fa92d
BorysGull <chayka2021@yahoo.com>
2022-08-01 19:29:15 +0300

    сделано два задания и первое без Esc

---
+++ src/js/01-gallery.js
@@ -0,0 +1,40 @@
+import {galleryItems} from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(galleryItem => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+       data-source="${galleryItem.original}"  
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+}).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const handleGallery = (evt) => {
+    evt.preventDefault();
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}"/>`);
+    instance.show();
+}
+gallery.addEventListener('click', handleGallery);
+
+
+
+
+
+
+
+
+
+
+
+
+
